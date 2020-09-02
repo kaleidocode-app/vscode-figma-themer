ed042bf6b6e1cd9fd2ff3ace49060c83d5509e22
Miguel Solorio <miguel.solorio@microsoft.com>
2020-09-02 15:42:37 -0700

    Add theme logic and structure

--- code.ts
+++ code.ts
@@ -1,33 +1,49 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+figma.showUI(__html__, { width: 350, height: 280 });
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
+let theme = [] as any
+let index = [] as any
 
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+for (let n of figma.currentPage.selection) {
+  if (n.type === 'FRAME') {
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+    // grab all token children
+    let children = n.findChildren(n => n.name.startsWith('$'))
+
+    children.forEach(c => {
+      let node = c as VectorNode
+
+      let colorName: string = node.name.substring(1);
+      let colors = node.fills[0].color
+      let colorHex = findTheHEX(colors.r, colors.g, colors.b)
+
+      if (index.includes(colorName) === false) {
+        index.push(colorName)
+        theme.push({
+          [colorName]: '#' + colorHex
+        });
+      };
+
+
+    })
+
+    // post in UI
+    figma.ui.postMessage({ type: 'loadStyles', theme: [theme] })
   }
+}
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+function findTheHEX(red: number, green: number, blue: number) {
+  var redHEX = rgbToHex(red)
+  var greenHEX = rgbToHex(green)
+  var blueHEX = rgbToHex(blue)
+
+  return redHEX + greenHEX + blueHEX
+}
+
+function rgbToHex(rgb: any) {
+  rgb = Math.floor(rgb * 255)
+  var hex = Number(rgb).toString(16)
+  if (hex.length < 2) {
+    hex = '0' + hex
+  }
+  return hex
+}
